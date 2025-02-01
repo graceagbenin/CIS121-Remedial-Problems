04681e4e00041a20d6f255789f4bc5d8057252a1
Grace Agbenin <ofureagbenin@gmail.com>
2025-01-31 18:28:44 -0600

    Initial commit
    Defense of Consolas

---
+++ DefenseOfConsolas/Program.cs
@@ -0,0 +1,40 @@
+﻿// Written by Grace Ofure Agbenin
+// 1/31/2025
+
+namespace DefenseOfConsolas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //modify console window title
+            Console.Title = "Defense of Consolas";
+
+            // input target row and column
+            Console.WriteLine("Enter Target Row:");
+            int targetRow = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Target Column:");
+            int targetColumn = int.Parse(Console.ReadLine());
+
+            // calculate deployment locations
+            int left = targetColumn - 1;
+            int right = targetColumn + 1;
+            int up = targetRow - 1;
+            int down = targetColumn - 1;
+
+            // Display deployment instructions 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Deploy to:");
+            Console.WriteLine($"({targetRow}, {left})");    // Left
+            Console.WriteLine($"({up}, {targetColumn})");   // Up
+            Console.WriteLine($"({targetRow}, {right})");   // Right
+            Console.WriteLine($"({down}, {targetColumn})");
+            Console.ResetColor();
+
+            //play Beep sound
+            Console.Beep(440, 1000);
+         
+        }
+    }
+}
