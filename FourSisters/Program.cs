b5a18a15addd26be37d591c3be0c3da675f65e0e
Grace Agbenin <ofureagbenin@gmail.com>
2025-01-31 22:40:12 -0600

    Initial Commit
    Initial commit

---
+++ FourSisters/Program.cs
@@ -0,0 +1,35 @@
+﻿// written by Grace Ofure
+//1/30/2025
+
+namespace FourSister
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Ask user for total egg count
+            Console.Write("Enter total number of eggs collected today:");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int totalEggs))
+            {
+                int eggsPerSister = totalEggs / 4;
+                int eggsForDuckbear = totalEggs % 4;
+
+
+                Console.WriteLine("Each sister gets: " + eggsPerSister);
+                Console.WriteLine("The duckbear gets: " + eggsForDuckbear);
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("Not a valid number. Try again");
+            }
+
+
+        }
+    }
+
+}
