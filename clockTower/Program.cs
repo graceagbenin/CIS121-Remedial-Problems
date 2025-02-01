ac96fdc2a5eab7966064e655bbc24d6da8663320
Grace Agbenin <ofureagbenin@gmail.com>
2025-01-31 18:32:19 -0600

    Initial Commit
    ClockTower

---
+++ clockTower/Program.cs
@@ -0,0 +1,30 @@
+﻿// Written by Grace Ofure Agbenin
+// 1/25/2025
+
+using System.ComponentModel.Design;
+
+namespace clockTower
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // input number
+            Console.Write("Input a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            // Determine if number is even or odd
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Tick");
+            }
+            else
+            {
+                Console.WriteLine("Tock");
+            }
+
+          
+        }
+    }
+}
