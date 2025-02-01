1bd13128ebcedfff1ab1186a044072c30a99cbba
Grace Agbenin <ofureagbenin@gmail.com>
2025-01-31 18:41:23 -0600

    Initial Commit
    Calculate Area of triangle

---
+++ TriangleFarmer/Program.cs
@@ -0,0 +1,23 @@
+﻿// Written by Grace Ofure Agbenin
+// 1/30/2025
+
+namespace TriangleFarmer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        // code for area of triangle
+        {  // Ask P-Thag for base and height
+            Console.Write("Enter Triangle base:");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Triangle Height:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double areaOfTriangle = (side1 * height) / 2.0;
+
+
+            Console.WriteLine("Area of Triangle:" + areaOfTriangle);
+        }
+    }
+}
